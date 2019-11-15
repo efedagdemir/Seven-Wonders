03dd6e0933271860cd5ab442c6f0c2f3bd88def3
sdaysegul <aysegul.gokce@ug.bilkent.edu.tr>
2019-11-15 21:06:45 +0300

    WonderBoard and WonderStage are implemented, however buildWonderStage method in WonderStage class is left to be implemented after Player and ModelService classes.

--- SWGame/src/model/WonderBoard.java
+++ SWGame/src/model/WonderBoard.java
@@ -2,4 +2,63 @@ package model;
 
 class WonderBoard {
 
+    private final int NUMBER_OF_STAGES = 3;
+    private int wonderLevel;
+    private Resource producedResource;
+    private String wonderName;
+    private WonderStage[] wonderStages;
+
+
+    WonderBoard(String wonderName,
+                Resource producedResource,
+                WonderStage... wonderStages
+    ) {
+        this.wonderLevel = 1;
+        setWonderName(wonderName);
+        setProducedResource(producedResource);
+        addWonderStages(wonderStages);
+    }
+
+    void buildWonderStage() {
+        getWonderStages()[wonderLevel - 1].buildWonderStage();
+        incrementWonderLevel();
+    }
+
+    private boolean addWonderStages(WonderStage[] wonderStages) {
+        if (wonderStages.length != NUMBER_OF_STAGES) {
+            return false;
+        }
+        this.wonderStages = wonderStages;
+        return true;
+    }
+
+    //setters and getters
+    public int getWonderLevel() {
+        return wonderLevel;
+    }
+
+    private void incrementWonderLevel() {
+        if (wonderLevel < NUMBER_OF_STAGES)
+            wonderLevel++;
+    }
+
+    public Resource getProducedResource() {
+        return producedResource;
+    }
+
+    private void setProducedResource(Resource producedResource) {
+        this.producedResource = producedResource;
+    }
+
+    public String getWonderName() {
+        return wonderName;
+    }
+
+    private void setWonderName(String wonderName) {
+        this.wonderName = wonderName;
+    }
+
+    public WonderStage[] getWonderStages() {
+        return wonderStages;
+    }
 }
--- SWGame/src/model/WonderStage.java
+++ SWGame/src/model/WonderStage.java
@@ -1,5 +1,56 @@
 package model;
 
-class WonderStage {
 
+class WonderStage {
+    private Resource[] providedResources;
+    private Resource[] requiredResources;
+    private VictoryPoints providedVictoryPoints;
+    private ConflictPoints providedConflictPoints;
+
+    WonderStage(Resource[] providedResources,
+                Resource[] requiredResources,
+                VictoryPoints victoryPoints,
+                ConflictPoints conflictPoints
+    ) {
+        setProvidedResources(providedResources);
+        setRequiredResources(requiredResources);
+        setProvidedVictoryPoints(victoryPoints);
+        setProvidedConflictPoints(conflictPoints);
+    }
+
+    void buildWonderStage() {
+        //TODO(need other classes to add wonder attributes to the player)
+    }
+
+    public Resource[] getProvidedResources() {
+        return providedResources;
+    }
+
+    private void setProvidedResources(Resource[] providedResources) {
+        this.providedResources = providedResources;
+    }
+
+    public Resource[] getRequiredResources() {
+        return requiredResources;
+    }
+
+    private void setRequiredResources(Resource[] requiredResources) {
+        this.requiredResources = requiredResources;
+    }
+
+    public VictoryPoints getProvidedVictoryPoints() {
+        return providedVictoryPoints;
+    }
+
+    private void setProvidedVictoryPoints(VictoryPoints providedVictoryPoints) {
+        this.providedVictoryPoints = providedVictoryPoints;
+    }
+
+    public ConflictPoints getProvidedConflictPoints() {
+        return providedConflictPoints;
+    }
+
+    public void setProvidedConflictPoints(ConflictPoints providedConflictPoints) {
+        this.providedConflictPoints = providedConflictPoints;
+    }
 }
