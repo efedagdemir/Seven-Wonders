5fa35a70d4530a711ed8650dc76c22610f6aa433
tuanaturkmen <merhabalar789>
2019-11-15 21:59:11 +0300

    Take a look

--- SWGame/src/model/AgeI.java
+++ SWGame/src/model/AgeI.java
@@ -1,4 +1,20 @@
 package model;
+import java.util.ArrayList;
 
 class AgeI extends Age {
+
+    @Override
+    void createDeck(int numOfPlayers) {
+
+
+        CivilianStructure Altar = new CivilianStructure(2, null, "Temple", null, 0);
+        CivilianStructure Theater = new CivilianStructure(2, null, "Statue", null, 0);
+        CivilianStructure Baths = new CivilianStructure(3, null, "Aqueduct", "Stone", 1 );
+
+        if (numOfPlayers == 4){
+
+            CivilianStructure Pawnshop = new CivilianStructure(3, null, null, null, 0);
+
+        }
+    }
 }
