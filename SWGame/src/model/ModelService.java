0f25f6e9bc30f58b8eb00f54708bd666734c8e28
sezinzeydan <41441283+sezinzeydan@users.noreply.github.com>
2019-11-17 17:46:14 +0300

    değişenler: item, player, modelservice

--- SWGame/src/model/ModelService.java
+++ SWGame/src/model/ModelService.java
@@ -3,13 +3,186 @@ package model;
 public class ModelService {
     private static ModelService modelService;
     Card xyz;
+    Age currentAge;
+    Player currentPlayer;
+    ImmutableList<Player>  playerList;
+    Card[][] rotating cardList;
+    ViewManipulator viewManipulator;
+    WonderBoard wonder;
+    WonderBoard[] wonderList;
+    int directionFactor;
 
     private ModelService(){}
-    Card sezo;
+
     public static ModelService getInstance() {
         if(modelService == null){
             modelService = new ModelService();
         }
         return modelService;
     }
+
+    public void updateCurrentAge(){
+        if(currentAge instanceof AgeI){
+            currentAge = new AgeII();
+        }
+       if(currentAge instanceof AgeII){
+            currentAge = new AgeII();
+        }
+        if(currentAge instanceof AgeIII){
+
+        }
+    }
+    //Will update the current player as each turn is played.
+    void updateCurrentPlayer(){
+
+    }
+    /*
+     Will remove the chosen card from the rotating card list.
+     The parameter of this method will be the card which is chosen by player.
+     */
+    void removeRotatingCardList(Card card){
+
+    }
+
+    /*
+    Will initiate the game. This method will call the createWonderBoard() method from the WonderBoard class
+    which will create the 7 wonder boards, the createDeck() method from the Age class
+    which will create the cards, the createPlayer() method from the Player class
+    which will create the player objects and the shuffle() method which will shuffle the wonder boards and cards.
+     */
+    void initializeGame(){
+
+    }
+
+    //Will shift the rotatingCardList when called and will make it turn according to the directionFactor attribute.
+    void rotateDecks(){
+
+    }
+
+    /*
+    Will calculate the final victory points of the players. It will call the updateVictoryPoints() method from the user
+    and then will call the showGameOverPage() method which will then call
+    the notifyGameOverPane() from the ViewManipulator.
+    */
+    void calculateFinalPoints(){
+
+    }
+
+    /*
+    Will calculate the victory points of the players after a conflict
+    and will call the notifyConflictScreen(Player[]) from the ViewManipulator class.
+    */
+    void initiateAndShowConflict(){
+
+    }
+
+    /*
+     Will call the showNextTurnPage() method which will then
+     provide the next player and call the notifyNextTurnScreen(Player) from the ViewManipulator.
+    */
+    void showNextTurnPage(){
+
+    }
+
+    /*
+    Will call the updateItemList() method which will change the items of the player
+    according to the taken action. Then will call the updateInfoPane(ItemList : Item) from the ViewManipulator.
+     */
+    void updateItemList(){
+
+    }
+
+    /*
+     Will call the notifyWonderPane() method from the ViewManipulator.
+    */
+    void buildWonder(){
+
+    }
+
+    /*
+     Will call the notifyHowToPlay() method from the ViewManipulator.
+     */
+    void showHowToPlay(){
+
+    }
+
+    /*
+    Will call the notifyGameOverPane() from the ViewManipulator.
+     */
+    void showGameOverPage(){
+
+    }
+
+    /*
+    Will call the notifyNameScreen() from the ViewManipulator.
+     */
+    void showNameScreen(){
+
+    }
+
+    /*
+    Will call the notifyCredits() from the ViewManipulator.
+    */
+    void showCredits(){
+
+    }
+
+    /*
+    Will call the notifyMainMenu() from the ViewManipulator.
+    */
+    void showMainMenu(){
+
+    }
+
+    /*
+    Will call the assignWonderBoard() which will assign wonder board objects to each player randomly,
+    the createRotatingCardList(WonderBoard[] wonderlist, Player[] playerList)
+    which will create the rotatingCardList, the assignName(String[] names)
+    which will assign the names of the players.
+    */
+    void assignGame(String[] names){
+
+    }
+
+    /*
+    Will randomly choose a player from the playerList
+    and then will randomly choose a wonder board object from the wonderList.
+    And then will assign that wonder board object to that player.
+    This process will be done for every player object in the playerList.
+     */
+    void assignWonderBoard(WonderBoard[] wonderList, Player[] playerList){
+
+    }
+
+    /*
+    Will divide the shuffled cardDeck object into 4 equal sub card sets
+    and then will put these into a card array which is called RotatingCardList.
+    */
+    Card[][] createRotatingCardList(){
+
+    }
+
+    /*
+    Will assign the given names to the names of the player objetcs.
+     */
+    void assignName(String[] names, Player[] playerList){
+
+    }
+
+    /*
+    Will call the notifyGameBorderPane(playerList: Player[], rotatingCardList : Card[][]) from the ViewManipulator.
+     */
+    void showGameScreen(){
+
+    }
+    /*
+    Creates 7 different WonderBoards according to their unique WonderStage’s and returns an array of these WonderBoards.
+     */
+    WonderBoard[] createWonderBoards(){
+
+    }
+
+
+
+
 }
