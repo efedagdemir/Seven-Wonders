5d2dc8e08e9502fb419f18efc8b11970187db878
firatyonak <firatyonak@gmail.com>
2019-11-18 17:36:17 +0300

    Merge branch 'unstable' of C:\Users\user\Desktop\cs3.sinif\CS319-1E-SW with conflicts.

--- SWGame/src/model/ViewManipulator.java
+++ SWGame/src/model/ViewManipulator.java
@@ -1,5 +1,56 @@
 package model;
 
-class ViewManipulator {
+import view.GameView;
 
+public class ViewManipulator {
+    private static ViewManipulator viewManipulator;
+    GameView gameView;
+    private ViewManipulator(){};
+    public  static ViewManipulator getInstance() {
+        if(viewManipulator == null){
+            viewManipulator = new ViewManipulator();
+        }
+        return viewManipulator;
+    }
+
+    void notifyMainMenu(){
+
+    }
+
+    void notifyNameScreen(){
+
+    }
+
+    void notifyNextTurnScreen( Player current){
+
+    }
+
+    void notifyConflictScreen( Player [] playerList){
+
+    }
+
+    void notifyCredits(){
+
+    }
+
+    void notifyHowToPlay(){
+
+    }
+
+    void updateInfoPane( Item [] itemList){
+
+    }
+
+    void notifyWonderPane( WonderBoard [] wonders){
+
+    }
+
+    void notifyGameOverPane( Player [] playerList){
+
+    }
+
+    void notifyGameBorderPane( Player [] playerList, Card[][] cards){
+
+
+    }
 }
