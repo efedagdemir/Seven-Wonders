bcf80907d8b5fd764e35643e3d9e7f9b100cac55
tuanaturkmen <merhabalar789>
2019-11-18 11:10:22 +0300

    Age 1 Card initializations

--- SWGame/src/model/RawMaterial.java
+++ SWGame/src/model/RawMaterial.java
@@ -1,12 +1,21 @@
 package model;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class RawMaterial extends Card {
 
     Coin price;
     Resource[] products;
+    Image image;
+    ImageView iv;
 
-    public RawMaterial(int amount, String[] prName, int[] prNo){
+    public RawMaterial(int amount, String[] prName, int[] prNo, String img){
 
+        image = new Image(img);
+        iv = new javafx.scene.image.ImageView();
+        iv.setImage(image);
+        iv.setFitHeight(100);
+        iv.setFitWidth(65);
         price = new Coin(amount);
 
         products = new Resource[prName.length];
