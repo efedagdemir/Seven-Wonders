5ef85827003271b47fc696eb7c096cd9d653f9be
tuanaturkmen <merhabalar789>
2019-11-18 11:10:14 +0300

    Age 1 Card initializations

--- SWGame/src/model/MilitaryStructure.java
+++ SWGame/src/model/MilitaryStructure.java
@@ -1,13 +1,24 @@
 package model;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class MilitaryStructure extends Card {
 
     Resource[] requiredProducts;
     Structure requiredStructure;
     Structure providedStructure;
     ConflictPoints conflictPoints;
+    Image image;
+    ImageView iv;
 
-    public MilitaryStructure(int mf, String rSt, String pSt, String[] gPName, int[] gPNo){
+    public MilitaryStructure(int mf, String rSt, String pSt, String[] gPName, int[] gPNo, String img){
+
+        image = new Image(img);
+        iv = new javafx.scene.image.ImageView();
+        iv.setImage(image);
+        iv.setFitHeight(100);
+        iv.setFitWidth(65);
 
         conflictPoints = new ConflictPoints(mf);
         requiredStructure = new Structure(rSt);
