db8ca53a0946c11152571642be5d5fe4f5492a36
tuanaturkmen <merhabalar789>
2019-11-21 20:21:20 +0300

    UpdateCoin

--- SWGame/src/view/SellCardDropBoard.java
+++ SWGame/src/view/SellCardDropBoard.java
@@ -4,6 +4,6 @@ import model.ModelService;
 
 public class SellCardDropBoard extends DropBoard {
     public void takeCardAction() {
-        ModelService.getInstance().getCurrentPlayer().addCoin(3);
+        ModelService.getInstance().getCurrentPlayer().updateCoin(3);
     }
 }
