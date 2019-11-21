4b4a828a64e8b83db1992a33dc1e70f7836fac9e
sdaysegul <aysegul.gokce@ug.bilkent.edu.tr>
2019-11-21 20:15:13 +0300

    GamePane update method

--- SWGame/src/view/GamePane.java
+++ SWGame/src/view/GamePane.java
@@ -1,27 +1,42 @@
 package view;
 
-import controller.ControllerFacade;
-import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+//import javafx.scene.layout.VBox;
 
 public class GamePane extends BorderPane {
-    public AllOpponentsPane allOpponentsPane = new AllOpponentsPane();
-    //public OpponentInfoPane opponentInfoPane = new OpponentInfoPane();
-    public PlayerInfoPane playerInfoPane = new PlayerInfoPane();
-    public CardActionPane cardActionPane = new CardActionPane();
-    public ResourcePane resourcePaneLeft = new ResourcePane();
-    public ResourcePane resourcePaneRight = new ResourcePane();
+    //    static GamePane gamePane;
+    //public Button startButton = new Button("dknfs");
+    public AllOpponentsPane allOpponentsPane;
+    public PlayerInfoPane playerInfoPane;
+    public CardActionPane cardActionPane;
+    public ResourcePane resourcePaneLeft;
+    public ResourcePane resourcePaneRight;
 
 
-//    ControllerFacade controllerFacade = ControllerFacade.getInstance();
+//    public static GamePane getInstance(){
+//        if(gamePane == null){
+//            gamePane = new GamePane();
+//        }
+//        return gamePane;
+//    }
 
     public GamePane() {
+        allOpponentsPane = new AllOpponentsPane();
+        playerInfoPane = new PlayerInfoPane();
+        cardActionPane = new CardActionPane();
+        resourcePaneLeft = new ResourcePane();
+        resourcePaneRight = new ResourcePane();
         setPrefSize(1300, 750);
         setTop(allOpponentsPane);
         setBottom(playerInfoPane);
         setLeft(resourcePaneLeft);
         setRight(resourcePaneRight);
         setCenter(cardActionPane);
+
+    }
+
+    public void update() {
+        playerInfoPane.update();
     }
 
 }
