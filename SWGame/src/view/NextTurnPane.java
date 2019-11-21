07ace773c293b6d4fb32426991061ee2e34d6755
tuanaturkmen <merhabalar789>
2019-11-21 21:32:07 +0300

    Build Wonder Stage

--- SWGame/src/view/NextTurnPane.java
+++ SWGame/src/view/NextTurnPane.java
@@ -1,5 +1,6 @@
 package view;
 
+import controller.ControllerFacade;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -19,19 +20,19 @@ public class NextTurnPane extends BorderPane {
         buttonBox.getChildren().add(okButton);
         setPrefSize(1300, 750);
         buttonBox.setStyle("-fx-font-size: 30px");
-        //okButton.setLayoutX(1100);
-        //okButton.setLayoutY(650);
+        okButton.setLayoutX(1100);
+        okButton.setLayoutY(650);
         setCenter(nextTurnLabel);
         setBottom(buttonBox);
         nextTurnLabel.setTextAlignment(TextAlignment.CENTER);
-        //setCenter(nextTurnLabel);
-        //setBottom(okButton);
+        setCenter(nextTurnLabel);
+        setBottom(okButton);
         nextTurnLabel.setFont(new Font(30));
         nextTurnLabel.setStyle("-fx-text-fill: #dfabdd;");
         okButton.setPrefSize(100, 50);
         nextTurnLabel.setAlignment(Pos.CENTER);
         buttonBox.setAlignment(Pos.BOTTOM_RIGHT);
-        //okButton.setOnAction(e -> ControllerFacade.getInstance().commandModel(e));
+        okButton.setOnAction(e -> ControllerFacade.getInstance().commandModel(e));
 
     }
 }
