e6862c426fb359b65ced611e43045d435f2dac7f
efedagdemir <56072318+efedagdemir@users.noreply.github.com>
2019-12-20 11:02:07 +0300

    update

--- SWGame/src/Server/ServerController/ServerControllerFacade.java
+++ SWGame/src/Server/ServerController/ServerControllerFacade.java
@@ -1,4 +1,21 @@
 package Server.ServerController;
 
+import Server.ServerManager;
+
 public class ServerControllerFacade {
+    private static ServerControllerFacade serverControllerFacade = new ServerControllerFacade();
+
+    private ServerManager serverManager;
+
+    public static ServerControllerFacade getInstance(){
+        return serverControllerFacade;
+    }
+
+    public ServerManager getServerManager() {
+        return serverManager;
+    }
+
+    public void setServerManager(ServerManager serverManager) {
+        this.serverManager = serverManager;
+    }
 }
